69e97e036415b06714e1e87e9a72e647ccdac616
Kenneth Dearman <164975838+kd00250@users.noreply.github.com>
2026-02-09 19:03:48 -0500

    Added Initial code sample for the project

---
+++ SE2-project-repos-sample/code/JavaFXSample (1)/HelloWorld MVVM/src/main/java/edu/westga/cs3211/helloworld/view/GreetingCodeBehind.java
@@ -0,0 +1,62 @@
+package edu.westga.cs3211.helloworld.view;
+
+import edu.westga.cs3211.helloworld.viewmodel.GreetingViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * The Class CodeBehind.
+ * 
+ * @author CS 3211
+ * @version Fall 2025
+ */
+public class GreetingCodeBehind {
+
+	@FXML
+	private TextField nameTextField;
+
+	@FXML
+	private Button submitButton;
+
+	@FXML
+	private Label greetingLabel;
+	
+	@FXML
+    private TextArea namesTextArea;
+
+	private GreetingViewModel viewModel;
+
+	/**
+	 * Instantiates a new greeting code behind.
+	 * 
+	 * @precondition none
+	 * @precondition none
+	 */
+	public GreetingCodeBehind() {
+		this.viewModel = new GreetingViewModel();
+	}
+
+	@FXML
+	void initialize() {
+
+		this.bindComponentsToViewModel();
+	}
+
+	private void bindComponentsToViewModel() {
+		this.greetingLabel.textProperty().bind(this.viewModel.greetingProperty());
+		this.nameTextField.textProperty().bindBidirectional(this.viewModel.nameProperty());
+		this.namesTextArea.textProperty().bind(this.viewModel.allNamesProperty());
+	}
+
+	@FXML
+	void handleSubmit() {
+
+		this.viewModel.sayGreeting();
+
+	}
+
+}
---
+++ SE2-project-repos-sample/code/JavaFXSample (1)/HelloWorld MVVM/src/test/java/edu/westga/cs3211/helloworld/model/person/TestConstructor.java
@@ -0,0 +1,23 @@
+package edu.westga.cs3211.helloworld.model.person;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3211.helloworld.model.Person;
+
+class TestConstructor {
+
+	@Test
+	void testNullName() {
+		assertThrows(IllegalArgumentException.class, ()->{new Person(null);});
+	}
+
+	@Test
+	void testValidName() {
+		Person result = new Person("Bob");
+		
+		assertEquals("Bob", result.getName(), "checking the name of the person");
+	}
+
+}
