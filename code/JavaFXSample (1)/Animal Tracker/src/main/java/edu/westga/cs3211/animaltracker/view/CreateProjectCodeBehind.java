7b234472163b8254b86079caeb182729224807ca
Mark Rockers <mrocker1@my.westga.edu>
2026-02-11 18:47:57 -0500

    Created View, CodeBehind, and ViewModel of CreateProject
    Made View with Back Button, Project Name and Location TextFields, ListViews for Adding and Removing Scientist from Project, Buttons for adding and removing Scientists from a Project, and a Create Project button.
    Made CodeBehind with all FXML Objects and Events for buttons.
    Made ViewModel with ProjectName and projectLocation Properties with getters and setters.

---
+++ code/JavaFXSample (1)/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/CreateProjectCodeBehind.java
@@ -0,0 +1,113 @@
+package edu.westga.cs3211.animaltracker.view;
+
+import edu.westga.cs3211.animaltracker.viewmodel.CreateProjectViewModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/**
+ * The CreateProject CodeBehind.
+ *
+ * @author mrocker1
+ */
+public class CreateProjectCodeBehind {
+
+    @FXML
+    private Label projectNameLabel;
+
+    @FXML
+    private TextField projectNameTextField;
+
+    @FXML
+    private Label projectNameWarningLabel;
+
+    @FXML
+    private Label projectLocationLabel;
+
+    @FXML
+    private TextField projectLocationTextField;
+
+    @FXML
+    private Label projectLocationWarningLabel;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Label scientistToAddLabel;
+
+    //TODO Check to make sure that it will be Scientist/Correct Object
+    @FXML
+    private ListView<String> scientistToAddListView;
+
+    @FXML
+    private Button removeScientistToAddButton;
+
+    @FXML
+    private Label availableScientistLabel;
+
+    //TODO Check to make sure that it will be Scientist/Correct Object
+    @FXML
+    private ListView<String> availableScientistToAddListView;
+
+    @FXML
+    private Button addScientistToProjectButton;
+
+    @FXML
+    private Button createProjectButton;
+
+    private CreateProjectViewModel viewModel;
+
+    /**
+     * Instantiates a new CreateProjectCodeBehind.
+     *
+     * @pre none
+     * @post none
+     */
+    public CreateProjectCodeBehind() {
+        this.viewModel = new CreateProjectViewModel();
+    }
+
+    //TODO Fix Documentation to how event will actually work
+
+    /**
+     * Goes back to Landing Page.
+     *
+     * @param actionEvent the event
+     */
+    public void onBackButtonClick(ActionEvent actionEvent) {
+    }
+
+    //TODO Fix Documentation to how event will actually work
+
+    /**
+     * Handles when the Add Scientist button is clicked.
+     *
+     * @param actionEvent the event
+     */
+    public void onAddScientistToProjectClick(ActionEvent actionEvent) {
+    }
+
+    //TODO Fix Documentation to how event will actually work
+
+    /**
+     * Handles when the Remove Scientist button is clicked.
+     *
+     * @param actionEvent the event
+     */
+    public void onRemoveScientistToAddClick(ActionEvent actionEvent) {
+    }
+
+    //TODO Fix Documentation to how event will actually work
+
+    /**
+     * Handles when the Create Project button is clicked.
+     *
+     * @param actionEvent the event
+     */
+    public void onCreateProjectClick(ActionEvent actionEvent) {
+    }
+}
