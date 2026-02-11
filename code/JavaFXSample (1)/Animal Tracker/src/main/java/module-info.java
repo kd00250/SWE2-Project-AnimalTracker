bcbd51e62057b8bcd95261151320c37d0bebb99b
Matthew Shirley <mshirle5@my.westga.edu>
2026-02-11 18:26:38 -0500

    Connect code behind and add cancel alert
    Users will now be prompted by an alter to ensure that they actually want to cancel tag creation

--- code/JavaFXSample (1)/Animal Tracker/src/main/java/module-info.java
+++ code/JavaFXSample (1)/Animal Tracker/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module edu.westga.cs3211.animaltracker {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires java.desktop;
 
     opens edu.westga.cs3211.animaltracker.view to javafx.fxml;
     exports edu.westga.cs3211.animaltracker;
