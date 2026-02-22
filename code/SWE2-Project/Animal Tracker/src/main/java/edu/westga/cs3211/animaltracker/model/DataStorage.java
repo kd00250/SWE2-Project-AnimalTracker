5ee2b5df23a6ac514207e6976b36ff92b67fa25c
Kenneth R Dearman <kd00250@my.westga.edu>
2026-02-22 01:01:42 -0500

    Added DataStorage Class
    -Added hashmap for animals and projects the implementation is not done just got to a stopping point, also this commit is similar as I forgot to add files last commit

--- code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/model/DataStorage.java
+++ code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/model/DataStorage.java
@@ -1,4 +1,40 @@
 package edu.westga.cs3211.animaltracker.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * The data storage class.
+ */
 public class DataStorage {
+    private static ArrayList<Scientist> scientists;
+    private static HashMap<Integer, Animal> animals;
+    private static HashMap<Integer, Project> projects;
+
+    /**
+     * gets the scientists.
+     *
+     * @return the scientists
+     */
+    public static ArrayList<Scientist> getScientists() {
+        return scientists;
+    }
+
+    /**
+     * Gets the animals.
+     *
+     * @return the animals
+     */
+    public static HashMap<Integer, Animal> getAnimals() {
+        return animals;
+    }
+
+    /**
+     * gets the project.
+     *
+     * @return the projects
+     */
+    public static HashMap<Integer, Project> getProjects() {
+        return projects;
+    }
 }
