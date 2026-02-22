113973df38ec71cba33d1295dcb94409bfe618f6
Matthew Shirley <mshirle5@my.westga.edu>
2026-02-22 16:10:52 -0500

    Connect viewing project data and creation page
    Landing page now connects to the project components of the system.

--- code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/LandingPageCodeBehind.java
+++ code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/LandingPageCodeBehind.java
@@ -1,15 +1,46 @@
 package edu.westga.cs3211.animaltracker.view;
 
 import edu.westga.cs3211.animaltracker.model.login.request.LoginResponse;
+import edu.westga.cs3211.animaltracker.view.swap.PageInformation;
+import edu.westga.cs3211.animaltracker.viewmodel.CreateProjectViewModel;
 import edu.westga.cs3211.animaltracker.viewmodel.LandingPageViewModel;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class LandingPageCodeBehind {
     private LandingPageViewModel landingViewModel;
 
     @FXML
+    private AnchorPane mainPane;
+
+    @FXML
     void initialize() {
         this.landingViewModel = new LandingPageViewModel();
+    }
+
+    @FXML
+    void onCreateProjectClick(ActionEvent event) {
+        try {
+            CreateProjectCodeBehind controller = ViewSwapper.loadPageFromStage(PageInformation.CreateProjectPath, this.mainPane, PageInformation.CreateProjectTitle);
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void onViewProjectClick(ActionEvent event) {
+        try {
+            ViewProjectDataCodeBehind controller = ViewSwapper.loadPageFromStage(PageInformation.ViewProjectPath, this.mainPane, PageInformation.ViewProjectTitle);
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setAuthenticationSession(LoginResponse session) {
--- code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/swap/PageInformation.java
+++ code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/swap/PageInformation.java
@@ -3,4 +3,10 @@ package edu.westga.cs3211.animaltracker.view.swap;
 public class PageInformation {
     public static final String LandingPath = "view/LandingPage.fxml";
     public static final String LandingTitle = "Main Page";
+
+    public static final String CreateProjectPath = "view/createProject.fxml";
+    public static final String CreateProjectTitle = "Create Project";
+
+    public static final String ViewProjectPath = "view/viewProjectData.fxml";
+    public static final String ViewProjectTitle = "View Project";
 }
