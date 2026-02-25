909b3eab4e01e31ec21614a543a44585e8906239
Kenneth R Dearman <kd00250@my.westga.edu>
2026-02-24 23:57:40 -0500

    Created add user gui
    -created add user gui and codebehind

---
+++ code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/AddUserCodeBehind.java
@@ -0,0 +1,31 @@
+package edu.westga.cs3211.animaltracker.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddUserCodeBehind {
+
+    @FXML
+    private Button addUserButton;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void addUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
+
