9749cdc8fef92f3e6cade1027fb1a6492436bfb3
Kenneth R Dearman <kd00250@my.westga.edu>
2026-02-28 17:07:45 -0500

    Added the code behind for add user
    -Added methods for the codebehind for adding the user (still need to test)

---
+++ code/SWE2-Project/Animal Tracker/src/test/java/edu/westga/cs3211/animaltracker/model/DataStorageModel/TestIsUsernameValid.java
@@ -0,0 +1,28 @@
+package edu.westga.cs3211.animaltracker.model.DataStorageModel;
+
+import edu.westga.cs3211.animaltracker.model.DataStorage;
+import edu.westga.cs3211.animaltracker.model.Role;
+import edu.westga.cs3211.animaltracker.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestIsUsernameValid {
+    @BeforeEach
+    void setUp() {
+        DataStorage.reset();
+    }
+
+    @Test
+    void validUsername() {
+        assertTrue(DataStorage.isUsernameAvailable("Kaz"));
+    }
+
+    @Test
+    void invalidUsername() {
+        User user = new User("Kaz", "1234", Role.ADMIN);
+        DataStorage.getUsers().add(user);
+        assertFalse(DataStorage.isUsernameAvailable("Kaz"));
+    }
+}
