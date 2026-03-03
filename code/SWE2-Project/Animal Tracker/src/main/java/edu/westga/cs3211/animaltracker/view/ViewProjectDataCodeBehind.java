d2653b6e61e9e9837de5b8c10e3e252997d00cfa
Kenneth R Dearman <kd00250@my.westga.edu>
2026-03-02 23:02:52 -0500

    Connecting view project data gui
    -Connecting create view project data to view model
    -Added functionality for the view project data

--- code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/ViewProjectDataCodeBehind.java
+++ code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/ViewProjectDataCodeBehind.java
@@ -1,16 +1,17 @@
 package edu.westga.cs3211.animaltracker.view;
+
+import edu.westga.cs3211.animaltracker.model.Animal;
+import edu.westga.cs3211.animaltracker.model.AnimalClass;
+import edu.westga.cs3211.animaltracker.model.Project;
+import edu.westga.cs3211.animaltracker.model.login.request.auth.LoginResponse;
+import edu.westga.cs3211.animaltracker.model.login.service.ServerService;
+import edu.westga.cs3211.animaltracker.view.swap.PageInformation;
+import edu.westga.cs3211.animaltracker.viewmodel.ViewProjectDataViewModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 
-/**
- * The View Project Data Code Behind.
- */
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.IOException;
 
 /**
  * The view project data code behind.
@@ -18,10 +19,7 @@ import java.util.ResourceBundle;
 public class ViewProjectDataCodeBehind {
 
     @FXML
-    private ComboBox<?> animalClassComboBox;
-
-    @FXML
-    private Label animalLabel;
+    private ComboBox<AnimalClass> animalClassComboBox;
 
     @FXML
     private TextArea descriptionTextArea;
@@ -36,16 +34,13 @@ public class ViewProjectDataCodeBehind {
     private Label projectNameLabel;
 
     @FXML
-    private ComboBox<?> subjectComboBox;
+    private ComboBox<Animal> subjectComboBox;
 
     @FXML
     private Button backButton;
 
     @FXML
     private Button createTagButton;
-
-    @FXML
-    private Button getInformationButton;
 
     @FXML
     private Label tagIDLabel;
@@ -56,32 +51,88 @@ public class ViewProjectDataCodeBehind {
     @FXML
     private Label weightLabel;
 
+    private ViewProjectDataViewModel vm;
+
     @FXML
     void onBackButtonClick(ActionEvent event) {
+        try {
+            LandingPageCodeBehind controller = ViewSwapper.loadPageFromStage(
+                    PageInformation.LANDING_PATH,
+                    this.backButton,
+                    PageInformation.LANDING_TITLE
+            );
 
+            controller.setSession(
+                    this.vm.getSession(),
+                    this.vm.getServerService()
+            );
+
+        } catch (IOException e) {
+            this.displayErrorPopup("Failed to navigate back: " + e.getMessage());
+        }
     }
 
     @FXML
     void onCreateTagButtonClick(ActionEvent event) {
-
+        //Jake pass in the create tag page
     }
 
     @FXML
-    void onGetInformationButtonClick(ActionEvent event) {
-
+    void onAnimalClassChange(ActionEvent event) {
+        this.subjectComboBox.getItems().addAll(this.vm.getAnimalsByType(this.vm.getAnimalClassProperty().get()));
     }
 
+    @FXML
+    void onSubjectChange(ActionEvent event) {
+        this.vm.setAnimalStats(this.subjectComboBox.getSelectionModel().getSelectedItem());
+    }
 
     @FXML
     void visualize(ActionEvent event) {
         //Implementation coming in future sprint :)
-        //The map visualization of the data (a nice to have)
+        //The map visualization of the data (a nice to have (said in the sprint planning :) )
     }
 
-    @FXML
-    void initialize() {
-
+    private void displayErrorPopup(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
+
+    void setSession(LoginResponse session, ServerService server) {
+        if (session == null) {
+            throw new IllegalArgumentException("Session cannot be null");
+        }
+        if (server == null) {
+            throw new IllegalArgumentException("Server cannot be null");
+        }
+        this.vm.setSession(session, server);
+    }
+
+    void setProject(Project project) {
+        this.vm = new ViewProjectDataViewModel();
+        this.vm.setProject(project);
+        this.setUpBindings();
+    }
+
+    private void setUpBindings() {
+        this.vm.getAnimalClassProperty().bind(this.animalClassComboBox.valueProperty());
+        this.animalClassComboBox.getItems().addAll(AnimalClass.values());
+        this.animalClassComboBox.setValue(AnimalClass.values()[0]);
+        this.vm.getAnimalProperty().bind(this.subjectComboBox.valueProperty());
+        this.heightLabel.textProperty().bind(this.vm.getHeightProperty().asString());
+        this.weightLabel.textProperty().bind(this.vm.getWeightProperty().asString());
+        this.lengthLabel.textProperty().bind(this.vm.getLengthProperty().asString());
+        this.tagIDLabel.textProperty().bind(this.vm.getTagIDProperty().asString());
+        this.descriptionTextArea.textProperty().bind(this.vm.getDescriptionProperty());
+        this.projectNameLabel.textProperty().bind(this.vm.getProjectNameProperty());
+    }
+
+//    @FXML
+//    void initialize() {
+//        this.setUpBindings();
+//
+//    }
 
 }
 
--- code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/viewmodel/ViewProjectDataViewModel.java
+++ code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/viewmodel/ViewProjectDataViewModel.java
@@ -5,9 +5,9 @@ import edu.westga.cs3211.animaltracker.model.AnimalClass;
 import edu.westga.cs3211.animaltracker.model.Project;
 import edu.westga.cs3211.animaltracker.model.login.request.auth.LoginResponse;
 import edu.westga.cs3211.animaltracker.model.login.service.ServerService;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
+
+import java.util.List;
 
 /**
  * the view project data view model.
@@ -18,6 +18,12 @@ public class ViewProjectDataViewModel {
     private ObjectProperty<AnimalClass> animalClass;
     private ObjectProperty<Animal> animal;
     private ObjectProperty<Project> project;
+    private StringProperty projectName;
+    private DoubleProperty height;
+    private DoubleProperty weight;
+    private DoubleProperty length;
+    private IntegerProperty tagID;
+    private StringProperty description;
 
     /**
      * creates a new instance of view project view model.
@@ -26,7 +32,154 @@ public class ViewProjectDataViewModel {
         this.animal = new SimpleObjectProperty<>();
         this.animalClass = new SimpleObjectProperty<>();
         this.project = new SimpleObjectProperty<>();
+        this.projectName = new SimpleStringProperty("");
+        this.height = new SimpleDoubleProperty();
+        this.weight = new SimpleDoubleProperty();
+        this.length = new SimpleDoubleProperty();
+        this.tagID = new SimpleIntegerProperty();
+        this.description = new SimpleStringProperty("");
     }
 
-    //public get
+    /**
+     * gets the project name property.
+     *
+     * @return the project name property
+     */
+    public StringProperty getProjectNameProperty() {
+        return this.projectName;
+    }
+
+    /**
+     * gets the project name property.
+     *
+     * @return the height property
+     */
+    public DoubleProperty getHeightProperty() {
+        return this.height;
+    }
+
+    /**
+     * gets the weight property.
+     *
+     * @return the weight property
+     */
+    public DoubleProperty getWeightProperty() {
+        return this.weight;
+    }
+
+    /**
+     * gets the length property.
+     *
+     * @return the length property
+     */
+    public DoubleProperty getLengthProperty() {
+        return this.length;
+    }
+
+    /**
+     * gets the tagID property.
+     *
+     * @return the tagID property
+     */
+    public IntegerProperty getTagIDProperty() {
+        return this.tagID;
+    }
+
+    /**
+     * gets the description property.
+     *
+     * @return the description property
+     */
+    public StringProperty getDescriptionProperty() {
+        return this.description;
+    }
+
+    /**
+     * gets the selected animal class.
+     *
+     * @return the animal class
+     */
+    public ObjectProperty<AnimalClass> getAnimalClassProperty() {
+        return this.animalClass;
+    }
+
+    /**
+     * gets the animal.
+     *
+     * @return the animal
+     */
+    public ObjectProperty<Animal> getAnimalProperty() {
+        return this.animal;
+    }
+
+    /**
+     * gets the project.
+     *
+     * @return the project
+     */
+    public ObjectProperty<Project> getProjectProperty() {
+        return this.project;
+    }
+
+    /**
+     * sets the project.
+     *
+     * @param project the project to set
+     */
+    public void setProject(Project project) {
+        this.project.set(project);
+        this.projectName.set(project.getName());
+    }
+
+    /**
+     * sets the animals stats.
+     *
+     * @param animal the animal to get the stats from
+     */
+    public void setAnimalStats(Animal animal) {
+        this.height.set(animal.getHeight());
+        this.weight.set(animal.getWeight());
+        this.length.set(animal.getLength());
+        this.tagID.set(animal.getTagID());
+        this.description.set(animal.getDescription());
+    }
+
+    /**
+     * gets the animals by type.
+     *
+     * @param type the type of animal class the animal belongs too
+     * @return the list of animals of a specific type
+     */
+    public List<Animal> getAnimalsByType(AnimalClass type) {
+        return this.getProjectProperty().get().getAnimalsByType(type);
+    }
+
+    /**
+     * Sets the session for this view model.
+     *
+     * @param session the user's session
+     * @param server the server service
+     */
+    public void setSession(LoginResponse session, ServerService server) {
+        this.authSession = session;
+        this.serverService = server;
+    }
+
+    /**
+     * Gets the session information.
+     *
+     * @return the session
+     */
+    public LoginResponse getSession() {
+        return this.authSession;
+    }
+
+    /**
+     * Gets the server service.
+     *
+     * @return the server service
+     */
+    public ServerService getServerService() {
+        return this.serverService;
+    }
 }
