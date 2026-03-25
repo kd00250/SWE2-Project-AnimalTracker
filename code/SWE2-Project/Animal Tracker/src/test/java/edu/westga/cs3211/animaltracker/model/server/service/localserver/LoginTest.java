496d8faaa0ecc505f50f039c7849cc806390fb26
Mark Rockers <mrocker1@my.westga.edu>
2026-03-25 19:11:20 -0400

    Fixed Failing Tests
    Fixed the testValidLoginAttempt by clearing the Users and the Usernames.
    Fixed the TestSetProject by clearing the Projects and creating a new Project for the test to get.

--- code/SWE2-Project/Animal Tracker/src/test/java/edu/westga/cs3211/animaltracker/model/server/service/localserver/LoginTest.java
+++ code/SWE2-Project/Animal Tracker/src/test/java/edu/westga/cs3211/animaltracker/model/server/service/localserver/LoginTest.java
@@ -1,16 +1,31 @@
 package edu.westga.cs3211.animaltracker.model.server.service.localserver;
 
+import edu.westga.cs3211.animaltracker.model.DataStorage;
+import edu.westga.cs3211.animaltracker.model.Role;
+import edu.westga.cs3211.animaltracker.model.User;
 import edu.westga.cs3211.animaltracker.model.server.request.auth.LoginRequest;
+import edu.westga.cs3211.animaltracker.model.server.request.auth.LoginResponse;
 import edu.westga.cs3211.animaltracker.model.server.service.LocalServer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest {
+
+    @BeforeEach
+    void setUp() {
+        DataStorage.getUsers().clear();
+        DataStorage.getProjects().clear();
+        DataStorage.getUsernameMap().clear();
+    }
+
     @Test
     void testValidLoginAttempt() {
         var username = "Bob";
         var password = "1234";
+        var user = new User(username, password, Role.SCIENTIST);
+        DataStorage.getUsers().add(user);
         var request = new LoginRequest(username, password);
         var auth = new LocalServer();
         var response = auth.login(request);
