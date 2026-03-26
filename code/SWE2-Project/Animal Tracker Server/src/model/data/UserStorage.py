3fe7541530cb5f1ea659316a0a58f32095260a31
Matthew Shirley <mshirle5@my.westga.edu>
2026-03-26 13:47:50 -0400

    Fix token removal for users
    tokens are now removed properly when a user is deleted

--- code/SWE2-Project/Animal Tracker Server/src/model/data/UserStorage.py
+++ code/SWE2-Project/Animal Tracker Server/src/model/data/UserStorage.py
@@ -29,7 +29,7 @@ class UserStorage:
 
         return self._token_map[token]
 
-    def remove_user(self, username, token=None):
+    def remove_user(self, username):
         self._username_map.pop(username)
 
         user = None
@@ -37,8 +37,14 @@ class UserStorage:
             if user.username == username:
                 user = current_user
 
-        if token is not None:
-            self._token_map.pop(token)
-
         self._users.remove(user)
 
+    def _remove_token(self, username):
+        token_to_delete = None
+        for token in self._token_map.values():
+            found_user = self._username_map[token]
+            if found_user.username == username:
+                token_to_delete = token
+
+        if token_to_delete is not None:
+            self._token_map.pop(token_to_delete)
