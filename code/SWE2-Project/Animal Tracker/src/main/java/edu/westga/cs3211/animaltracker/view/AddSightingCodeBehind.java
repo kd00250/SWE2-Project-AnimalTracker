c4b41c92e6684edc35fb0ed17e00dee0ce521bee
Mark Rockers <mrocker1@my.westga.edu>
2026-04-21 15:20:42 -0400

    Fixed AddSighting Page
    Fixed the page to allow text input, bug was due to binding only one way instead of BiDirectional.
    Created Methods to Restrict Text Input via Regex, but could not figure out how to do Latitude and Longitude
    Need to Change the Sighting class still and fix the Save Sighting to use the request

--- code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/AddSightingCodeBehind.java
+++ code/SWE2-Project/Animal Tracker/src/main/java/edu/westga/cs3211/animaltracker/view/AddSightingCodeBehind.java
@@ -10,15 +10,19 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 /**
  * Code-behind (controller) for addSighting.fxml.
  */
 public class AddSightingCodeBehind {
 
+    private static final String ANIMAL_ID_REGEX_TEXT = "\\d{0,2}/?\\d{0,2}/?\\d{0,4}";
+    private static final String LONGITUDE_REGEX_TEXT = "([-]?1[0-7][0-9]|[0-9]?[0-9]\\.\\d+|180\\.0+?)";
+    private static final String LATITUDE_REGEX_TEXT = "([-]?[0-8]?[0-9]\\.\\d+|90\\.0+?)";
+
     @FXML
     private Button backButton;
-
     @FXML
     private TextField animalTextField;
     @FXML
@@ -53,17 +57,39 @@ public class AddSightingCodeBehind {
         this.minuteSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 0));
         this.vm = new AddSightingViewModel();
         this.bindAllProperties();
+        this.setTextInputRestrictions();
     }
 
     private void bindAllProperties() {
-        this.animalTextField.textProperty().bind(this.vm.animalIDProperty());
-        this.locationTextField.textProperty().bind(this.vm.locationProperty());
-        this.latitudeTextField.textProperty().bind(this.vm.latitudeProperty());
-        this.longitudeTextField.textProperty().bind(this.vm.longitudeProperty());
-        this.datePicker.accessibleTextProperty().bind(this.vm.dateProperty());
-        this.hourSpinner.accessibleTextProperty().bind(this.vm.hourProperty());
-        this.minuteSpinner.accessibleTextProperty().bind(this.vm.minuteProperty());
-        this.noteTextArea.textProperty().bind(this.vm.noteProperty());
+        this.animalTextField.textProperty().bindBidirectional(this.vm.animalIDProperty());
+        this.locationTextField.textProperty().bindBidirectional(this.vm.locationProperty());
+        this.latitudeTextField.textProperty().bindBidirectional(this.vm.latitudeProperty());
+        this.longitudeTextField.textProperty().bindBidirectional(this.vm.longitudeProperty());
+        this.datePicker.accessibleTextProperty().bindBidirectional(this.vm.dateProperty());
+        this.hourSpinner.accessibleTextProperty().bindBidirectional(this.vm.hourProperty());
+        this.minuteSpinner.accessibleTextProperty().bindBidirectional(this.vm.minuteProperty());
+        this.noteTextArea.textProperty().bindBidirectional(this.vm.noteProperty());
+    }
+
+    private void setTextInputRestrictions() {
+        this.setTextRestrictions(this.animalTextField, ANIMAL_ID_REGEX_TEXT);
+        this.datePicker.setEditable(false);
+    }
+
+    private void setTextRestrictions(TextField textField, String pattern) {
+        var formattedPattern = Pattern.compile(pattern);
+        var formatter = this.createInputPattern(formattedPattern);
+        textField.setTextFormatter(formatter);
+    }
+
+    private TextFormatter<String> createInputPattern(Pattern pattern) {
+        return new TextFormatter<String>(change -> {
+            if (pattern.matcher(change.getText()).matches()) {
+                return change;
+            } else {
+                return null;
+            }
+        });
     }
 
     @FXML
