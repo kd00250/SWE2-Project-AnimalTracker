ce8f7a24daaf274b2fc4498fd628de1db7aeaccd
Kenneth R Dearman <kd00250@my.westga.edu>
2026-04-21 00:09:38 -0400

    Changed Animal toString
    -Changed animal to string to show the animal tagID instead of the incrementing animal id

--- code/SWE2-Project/Animal Tracker/src/test/java/edu/westga/cs3211/animaltracker/model/AnimalModel/ConstructorTest.java
+++ code/SWE2-Project/Animal Tracker/src/test/java/edu/westga/cs3211/animaltracker/model/AnimalModel/ConstructorTest.java
@@ -27,7 +27,7 @@ class ConstructorTest {
         assertEquals(TagStatus.ACTIVE, animal.getTagStatus());
         assertEquals("", animal.getDescription());
         assertEquals(2, animal.getId());
-        assertEquals("Bird 2", animal.toString());
+        assertEquals("Bird 122345", animal.toString());
     }
 
     @Test
