d6b4f36ca77faf63092f4326fcbf70df4416b807
Matthew Shirley <mshirle5@my.westga.edu>
2026-04-23 05:01:00 -0400

    Add test to see data viewmodel
    Viewmodel for see data now has test for branches
    for code coverage

---
+++ code/SWE2-Project/Animal Tracker/src/test/java/edu/westga/cs3211/animaltracker/viewmodel/seeData/SeeDataViewmodel/FakeServerService.java
@@ -0,0 +1,80 @@
+package edu.westga.cs3211.animaltracker.viewmodel.seeData.SeeDataViewmodel;
+
+import edu.westga.cs3211.animaltracker.model.Project;
+import edu.westga.cs3211.animaltracker.model.Role;
+import edu.westga.cs3211.animaltracker.model.Sighting;
+import edu.westga.cs3211.animaltracker.model.User;
+import edu.westga.cs3211.animaltracker.model.server.request.auth.LoginRequest;
+import edu.westga.cs3211.animaltracker.model.server.request.auth.LoginResponse;
+import edu.westga.cs3211.animaltracker.model.server.request.data.*;
+import edu.westga.cs3211.animaltracker.model.server.service.ServerService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeServerService implements ServerService {
+    private final ArrayList<Sighting> sightings;
+    public FakeServerService() {
+        this.sightings = new ArrayList<>();
+    }
+    @Override
+    public List<Sighting> getSightings(GetSightingRequest request) {
+        return this.sightings;
+    }
+
+    @Override
+    public boolean addSighting(AddSightingRequest request) {
+        this.sightings.add(request.getSighting());
+        return true;
+    }
+
+    @Override
+    public Project requestSingleProject(GetSingleProjectRequest request) {
+        return null;
+    }
+
+    @Override
+    public boolean addUser(AddUserRequest request) {
+        return false;
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest request) {
+        return null;
+    }
+
+    @Override
+    public Role requestUserRole(UserDataRequest request) {
+        return null;
+    }
+
+    @Override
+    public List<Project> requestUserProjects(GetProjectRequest request) {
+        return List.of();
+    }
+
+    @Override
+    public void requestAddAnimal(AddAnimalRequest request) {
+
+    }
+
+    @Override
+    public List<User> requestAllScientist(UserDataRequest request) {
+        return List.of();
+    }
+
+    @Override
+    public List<User> requestAllScientistsFromServer(GetAllScientistsRequests request) {
+        return List.of();
+    }
+
+    @Override
+    public void AddProject(AddProjectRequest request) {
+
+    }
+
+    @Override
+    public void deleteProject(DeleteProjectRequest request) {
+
+    }
+}
---
+++ code/SWE2-Project/Animal Tracker/src/test/java/edu/westga/cs3211/animaltracker/viewmodel/seeData/SeeDataViewmodel/TestProperties.java
@@ -0,0 +1,70 @@
+package edu.westga.cs3211.animaltracker.viewmodel.seeData.SeeDataViewmodel;
+
+import edu.westga.cs3211.animaltracker.model.Animal;
+import edu.westga.cs3211.animaltracker.model.AnimalClass;
+import edu.westga.cs3211.animaltracker.model.Sighting;
+import edu.westga.cs3211.animaltracker.model.server.request.auth.LoginResponse;
+import edu.westga.cs3211.animaltracker.model.server.request.data.AddSightingRequest;
+import edu.westga.cs3211.animaltracker.model.server.service.ServerService;
+import edu.westga.cs3211.animaltracker.viewmodel.seeData.SeeDataViewModel;
+import javafx.beans.property.SimpleObjectProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestProperties {
+    private SeeDataViewModel seeDataViewModel;
+    private ServerService server;
+    private Animal animal;
+    @BeforeEach
+    void setUp() {
+        var vm = new SeeDataViewModel();
+        var animal0 = new Animal(AnimalClass.MAMMAL, 10, 10, 10, 10, "Bird thing");
+        var server = new FakeServerService();
+        this.seeDataViewModel = vm;
+        this.server = server;
+        this.animal = animal0;
+    }
+
+    @Test
+    void testInitializesWithNoSightings() {
+        this.seeDataViewModel.setAnimalProperty(new SimpleObjectProperty<>(this.animal));
+        this.seeDataViewModel.setSession(new LoginResponse("1234", 1234), this.server);
+        assertNotNull(this.seeDataViewModel.sightings());
+        assertEquals(0, this.seeDataViewModel.sightings().size());
+    }
+
+    @Test
+    void testMultipleSightingShouldLoad() {
+        var sighting0 = new Sighting(10,
+                "Florida", 10.6, 60.6,
+                LocalDateTime.of(2024, 4, 20,10, 40),
+                "Notes", "Bob");
+
+        this.server.addSighting(new AddSightingRequest("Token", sighting0));
+        this.seeDataViewModel.setAnimalProperty(new SimpleObjectProperty<>(this.animal));
+        this.seeDataViewModel.setSession(new LoginResponse("1234", 1234), this.server);
+        assertEquals(1, this.seeDataViewModel.sightings().size());
+
+        assertEquals("Florida", this.seeDataViewModel.sightings().get(0).locationProperty().getValue());
+        assertEquals(10.6, this.seeDataViewModel.sightings().get(0).latitudeProperty().getValue());
+        assertEquals(60.6, this.seeDataViewModel.sightings().get(0).longitudeProperty().getValue());
+        assertEquals("Bob", this.seeDataViewModel.sightings().get(0).usernameProperty().getValue());
+    }
+
+    @Test
+    void testGetServerSessions() {
+        this.seeDataViewModel.setAnimalProperty(new SimpleObjectProperty<>(this.animal));
+
+        var response = new LoginResponse("1234", 1234);
+        var server = this.server;
+        this.seeDataViewModel.setSession(response, server);
+        assertEquals(server, this.seeDataViewModel.getServerService());
+        assertEquals(response, this.seeDataViewModel.getSession());
+    }
+
+
+}
